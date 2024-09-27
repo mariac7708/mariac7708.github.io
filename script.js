5fa3c42f59d060dff7d213a412ad33560740f3ee
mariac7708 <mariac338@nycstudents.net>
2024-09-27 13:28:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Maria Camacho"; // update the part BETWEEN the "quotes"
+let userName = "mariac7708"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
